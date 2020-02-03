8229bdcb4e92a81b35154383032890d59f6bbc00
Esten Rye <esten.rye@ryezone.com>
2020-02-03 07:34:13 -0600

    Work in progress.
    implemented sort interface for swarm tasks
    Need to implement search interface.

--- internal/types/services.go
+++ internal/types/services.go
@@ -1,6 +1,9 @@
 package types
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"github.com/docker/docker/api/types/swarm"
+)
 
 // service representation
 type Service struct {
@@ -14,6 +17,48 @@ type Service struct {
 type ServiceList []Service
 
 func (l *ServiceList) ToJSON() (string, error) {
+	formatted, err := json.MarshalIndent(l, "", "  ")
+	if err != nil {
+		return "[]", err
+	}
+	return string(formatted), nil
+}
+
+// Sort interface for Docker Swarm Tasks
+type TaskList []swarm.Task
+
+// Implementation of the Less function of the golang sort interface
+func (t TaskList) Less(i, j int) bool {
+	return t[i].ServiceID < t[j].ServiceID && t[i].ID < t[j].ID
+}
+
+// Implementation of the Swap function of the golang sort interface
+func (t TaskList) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+// Implementation of the Len function of the golang sort interface
+func (t TaskList) Len() int {
+	return t.Len()
+}
+
+type TaskSummary struct {
+	Id           string           `json:",omitempty"`
+	Status       swarm.TaskStatus `json:",omitempty"`
+	DesiredState swarm.TaskState  `json:",omitempty"`
+}
+
+type ServiceSummary struct {
+	Name     string            `json:",omitempty"`
+	Image    string            `json:",omitempty"`
+	Tag      string            `json:",omitempty"`
+	Mode     swarm.ServiceMode `json:",omitempty"`
+	TaskList []TaskSummary     `json:",omitempty"`
+}
+
+type ServiceSummaryList []ServiceSummary
+
+func (l *ServiceSummaryList) ToJSON() (string, error) {
 	formatted, err := json.MarshalIndent(l, "", "  ")
 	if err != nil {
 		return "[]", err
