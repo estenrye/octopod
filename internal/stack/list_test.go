47f1b3699f9d5fed4b3622a1e3c4fe330107007f
Esten Rye <esten.rye@ryezone.com>
2020-07-22 06:25:51 +0000

    fix expected value

--- internal/stack/list_test.go
+++ internal/stack/list_test.go
@@ -11,7 +11,7 @@ func TestListStacks(t *testing.T) {
 		{
 			Name:  "testing_whoami_1",
 			Image: "containous/whoami",
-			Tag:   "latest@sha256:c0d68a0f9acde95c5214bd057fd3ff1c871b2ef12dae2a9e2d2a3240fdd9214b",
+			Tag:   "c0d68a0f9acde95c5214bd057fd3ff1c871b2ef12dae2a9e2d2a3240fdd9214b",
 			Labels: map[string]string{
 				"com.docker.stack.image":        "containous/whoami",
 				"com.docker.stack.namespace":    "testing",
@@ -24,7 +24,7 @@ func TestListStacks(t *testing.T) {
 		{
 			Name:  "testing_whoami_2",
 			Image: "containous/whoami",
-			Tag:   "latest@sha256:c0d68a0f9acde95c5214bd057fd3ff1c871b2ef12dae2a9e2d2a3240fdd9214b",
+			Tag:   "c0d68a0f9acde95c5214bd057fd3ff1c871b2ef12dae2a9e2d2a3240fdd9214b",
 			Labels: map[string]string{
 				"com.docker.stack.image":        "containous/whoami",
 				"com.docker.stack.namespace":    "testing",
