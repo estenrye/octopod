3b4de36d81dd4ca70c34138f0ed8d2dd3e3edf23
Esten Rye <esten.rye@ryezone.com>
2020-07-22 06:32:33 +0000

    fix com.docker.stack.image label in expected output.

--- internal/stack/list_test.go
+++ internal/stack/list_test.go
@@ -13,7 +13,7 @@ func TestListStacks(t *testing.T) {
 			Image: "containous/whoami@sha256",
 			Tag:   "c0d68a0f9acde95c5214bd057fd3ff1c871b2ef12dae2a9e2d2a3240fdd9214b",
 			Labels: map[string]string{
-				"com.docker.stack.image":        "containous/whoami",
+				"com.docker.stack.image":        "containous/whoami@sha256:c0d68a0f9acde95c5214bd057fd3ff1c871b2ef12dae2a9e2d2a3240fdd9214b",
 				"com.docker.stack.namespace":    "testing",
 				"io.company.variables.vargroup": "2019.08.09",
 			},
@@ -26,7 +26,7 @@ func TestListStacks(t *testing.T) {
 			Image: "containous/whoami@sha256",
 			Tag:   "c0d68a0f9acde95c5214bd057fd3ff1c871b2ef12dae2a9e2d2a3240fdd9214b",
 			Labels: map[string]string{
-				"com.docker.stack.image":        "containous/whoami",
+				"com.docker.stack.image":        "containous/whoami@sha256:c0d68a0f9acde95c5214bd057fd3ff1c871b2ef12dae2a9e2d2a3240fdd9214b",
 				"com.docker.stack.namespace":    "testing",
 				"io.company.variables.vargroup": "2019.08.09",
 			},
